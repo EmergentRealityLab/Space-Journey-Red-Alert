cb893803a788b12f6e834829b4b989d55e786c8d
Chris <cloutc@rpi.edu>
2016-03-25 14:53:59 -0400

    Setup basic ship rotation and thrusting

---
+++ SpaceshipBridge/Assets/Scripts/Engines.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Engines : MonoBehaviour {
+
+    [Range(-1f, 1f)] public float pitch;
+    [Range(-1f, 1f)] public float yaw;
+    [Range(-1f, 1f)] public float roll;
+    [Range(-1, 1)]    public float thrust;
+
+    Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(pitch, yaw, roll);
+        rb.AddForce(transform.forward * thrust);
+	}
+}
