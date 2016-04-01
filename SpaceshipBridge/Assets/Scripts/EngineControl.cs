02584eed71ffbf7b35a93ba49e280a8c686a4539
Parshva Shah <g7parsh@gmail.com>
2016-04-01 17:12:33 -0400

    (Parsh) Started some tests

---
+++ SpaceshipBridge/Assets/Scripts/EngineControl.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EngineControl : MonoBehaviour {
+	public Engines engine;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetMouseButtonDown(0)){
+			if (Camera.main == null) {
+				print ("Shit's fucked yo");
+			}
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+
+		
+			if(Physics.Raycast(ray, out hit)){
+				print("hi");
+			}
+		}
+
+	
+	}
+
+
+}
